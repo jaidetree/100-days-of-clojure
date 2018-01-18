1806308b6b18218fecf8067516f98dd2c03b6938
Jay Zawrotny <jayzawrotny@gmail.com>
2018-01-18 02:25:18 -0500

    Updated docs & formatting in day-5 project

--- 5-core-async/src/async_reddit.clj
+++ 5-core-async/src/async_reddit.clj
@@ -4,23 +4,28 @@
 
 (defn posts->chan
   [posts]
-  (async/to-chan posts))
+  (-> (async/to-chan posts)
+      (async/pipe (async/chan 5 (map :data)))))
 
 (defn get-post-link
+  "Parse link from a reddit post"
   [post]
   (str "https://reddit.com" (:permalink post)))
 
 (defn get-post-footer
+  "Parse footer from a reddit post"
   [post]
   (str (:score post) " | " (:num_comments post) " comments"))
 
 (defn print-post
+  "Display a reddit post to console"
   [post]
   (doseq [display-fn [:title get-post-link get-post-footer]]
     (println (display-fn post)))
   (println "\n"))
 
 (defn print-posts
+  "Displays channel of reddit posts to console"
   [posts-chan]
   (async/go-loop []
     (when-let [post (async/<! posts-chan)]
@@ -28,9 +33,8 @@
       (recur))))
 
 (defn -main
+  "Get 5 of the latest reddit posts and print them to console"
   []
   (->> (get-reddit-posts "limit=5")
        (posts->chan)
        (print-posts)))
-
-(-main)
