d914f1c73118a90852c408d02da31d09ec4d8807
Jay Zawrotny <jayzawrotny@gmail.com>
2018-01-21 01:08:46 -0500

    Finished day 9 - Yoanna watches

---
+++ 9-better-async-reddit/src/async_reddit.clj
@@ -0,0 +1,33 @@
+(ns async-reddit
+  (:require [reddit :refer [get-reddit-posts]]
+            [clojure.core.async :as async]))
+
+(defn get-post-link
+  [post]
+  (str "https://reddit.com" (:permalink post)))
+
+(defn get-post-footer
+  [post]
+  (str (:score post) " | " (:num_comments post) " comments"))
+
+(defn print-post
+  [post]
+  (doseq [display-fn [:title get-post-link get-post-footer]]
+    (println (display-fn post)))
+  (println "\n"))
+
+(defn sink
+  "Takes a side-effect function and a channel and calls the side effect on
+  each item in the channel."
+  [each-fn channel]
+  (async/go-loop []
+    (when-let [item (async/<! channel)]
+      (each-fn item)
+      (recur))))
+
+(defn -main
+  []
+  (def posts-channel (async/chan 1 (map :data)))
+  (sink print-post posts-channel)
+  (async/onto-chan posts-channel
+    (get-reddit-posts "limit=5")))
