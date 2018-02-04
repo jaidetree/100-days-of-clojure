7d7311a8b23ad9d018ba511698ef11df2dc3a87f
Jay Zawrotny <jayzawrotny@gmail.com>
2018-02-03 23:13:46 -0500

    Added some error handling to pizza

--- 22-readline/src/reader.clj
+++ 22-readline/src/reader.clj
@@ -1,10 +1,22 @@
 (ns reader)
 
+(declare pizza-prompt)
+
 (defn count-pizzas
   [n-slices-str]
-  (let [n-slices (Integer. n-slices-str)]
-    (int (+ 0.5 (/ n-slices 8)))))
+  (try
+    (let [n-slices (Integer. n-slices-str)]
+      (int (+ 0.5 (/ n-slices 8))))
+    (catch Exception e
+      (println "I didn't catch that.")
+      (pizza-prompt)
+      nil)))
+
+(defn pizza-prompt
+  []
+  (println "How many pizza slices?")
+  (when-let [pizzas (count-pizzas (read-line))]
+    (println (str "Ok, you'll need " pizzas " pizzas"))))
 
 (defn -main []
-  (println "How many pizza slices?")
-  (println (str "Ok, you'll need " (count-pizzas (read-line)) " pizzas")))
+  (pizza-prompt))
