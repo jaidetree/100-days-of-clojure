c9086184d8cc2184b238b5b703ffdd45171a3554
Jay Zawrotny <jayzawrotny@gmail.com>
2018-03-09 00:11:08 -0500

    Updated terminal to make it more portable

--- 54-terminal/src/terminal.clj
+++ 54-terminal/src/terminal.clj
@@ -4,14 +4,16 @@
 (defn read-tty!
   "Returns the stdout of the stty -a command in current terminal"
   []
-  (->> (shell/sh "/bin/sh" "-c" "stty -a < /dev/tty")
-       :out))
-
-(defn get-terminal-size
-  "Returns a map with {:rows n :columns n} of current terminal"
-  []
-  (let [tty-str (read-tty!)
-        [s & dimensions] (re-find #"(\d+) rows; (\d+) columns;" tty-str)
-        [rows columns] (map #(Integer. %) dimensions)]
+  (let [out (:out (shell/sh "/bin/sh" "-c" "stty -a < /dev/tty"))]
     (shutdown-agents)
+    out))
+
+(defn parse-size
+  "Returns a map with {:rows n :columns n} of current terminal"
+  [tty-str]
+  (let [[s & dimensions] (re-find #"(\d+) rows; (\d+) columns;" tty-str)
+        [rows columns] (map #(Integer. %) dimensions)]
     {:rows rows :columns columns}))
+
+(defn get-size []
+  (-> (read-tty!) (parse-size)))
--- 54-terminal/src/terminal_test.clj
+++ 54-terminal/src/terminal_test.clj
@@ -2,12 +2,19 @@
   (:require [clojure.test :refer :all]
             [terminal :refer :all]))
 
-(deftest get-terminal-size-test
+(deftest get-size-test
   (testing "Returns map with {:rows n :columns n}"
-    (let [size (get-terminal-size)]
+    (let [size (get-size)]
       (is (map? size))
       (is (number? (:rows size)))
       (is (number? (:columns size))))))
+
+(deftest parse-size-test
+  (testing "Returns map with {:rows n :columns n}"
+    (let [size (parse-size "24 rows; 32 columns;")]
+      (is (map? size))
+      (is (= (:rows size) 24))
+      (is (= (:columns size) 32)))))
 
 (defn -main
   []
