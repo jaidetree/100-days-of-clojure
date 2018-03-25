e7c0c0389a393ac6b65cd3850d0f26c4d16b36d9
Jay Zawrotny <jayzawrotny@gmail.com>
2018-03-25 02:06:32 -0400

    Day 71 - Finally got mta data loading and NyctSubway class compiled to .class file :D

---
+++ 71-mta-java-class/README.md
@@ -0,0 +1,5 @@
+# MTA Transit Info
+Attempts to parse real-time subway data from MTA api.
+
+## References
+- https://github.com/kushpatel/MetroSub/blob/b3a76883efc9f4c04c8465fd24532bf45971b5f6/src/com/MetroSub/datamine/GtfsFeed.java
---
+++ 71-mta-java-class/src/mta.clj
@@ -0,0 +1,34 @@
+(ns mta
+  (:require [clj-http.client :as client]
+            [cheshire.core :as json])
+  (:import (com.google.transit.realtime GtfsRealtime$FeedMessage NyctSubway)))
+
+(defn get-config
+  [filename]
+  (read-string (slurp filename)))
+
+(defn str->feed
+  [feed-str]
+  (->> (GtfsRealtime$FeedMessage/parseFrom feed-str)
+       (.getEntityList)))
+
+(defn get-url
+  [config]
+  (format "%s?key=%s&feed_id=16"
+    (:url config)
+    (:token config)))
+
+(defn -main []
+  (spit "data.secret.txt" "")
+  (let [config (get-config "dev.secret.edn")]
+    (as-> (client/get (get-url config) {:as :byte-array}) $
+          (:body $)
+          (str->feed $)
+          (filter #(.hasTripUpdate %) $)
+          (map #(.getTripUpdate %) $)
+          (map #(.getTrip %) $)
+          (map #(.getTripId %) $)
+          (take 1 $)
+          (doseq [item $]
+            (println item)
+            (spit "data.secret.txt" item :append true)))))
