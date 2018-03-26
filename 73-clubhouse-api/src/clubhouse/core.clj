c390fe98ccf38744191b3cc2a3e4f37474441d53
Jay Zawrotny <jayzawrotny@gmail.com>
2018-03-26 19:26:26 -0400

    Day 73 - Fetching stories & workflows from clubhouse

--- 73-clubhouse-api/src/clubhouse/core.clj
+++ 73-clubhouse-api/src/clubhouse/core.clj
@@ -1,6 +1,43 @@
-(ns clubhouse.core)
+(ns clubhouse.core
+  (:require [clj-http.client :as client]
+            [clojure.pprint :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def config (read-string (slurp "dev.secret.edn")))
+
+(defn fetch-clubhouse-workflows
+  []
+  (:body
+   (client/get
+    (format "https://api.clubhouse.io/api/v2/workflows?token=%s"
+            (:clubhouse_api_token config))
+    {:as :json})))
+
+(defn fetch-clubhouse-stories
+  []
+  (:body
+   (client/request
+    {:headers {}
+     :method "GET"
+     :url (format "https://api.clubhouse.io/api/v2/search/stories?token=%s"
+                  (:clubhouse_api_token config))
+     :accepts :json
+     :content-type :json
+     :as :json
+     :body "{\"page_size\": 3, \"query\": \"owner:jayzawrotny state:500008928\"}"})))
+
+(defn parse-workflow-states
+  [workflows]
+  (->> workflows
+        (take 1)
+        (mapcat :states)
+        (map #(hash-map (:name %) (:id %)))))
+
+
+(defn -main
+  []
+  (pprint
+   (->> (fetch-clubhouse-stories)
+        :data
+        (map :name))))
+
+(-main)
