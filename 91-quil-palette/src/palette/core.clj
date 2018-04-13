ebc06efe59932461cb1ecbcd9d8e26f85a44a764
Jay Zawrotny <jayzawrotny@gmail.com>
2018-04-13 10:16:24 -0400

    Day 91 - Quil makeup palette for my lady friend

---
+++ 91-quil-palette/src/palette/core.clj
@@ -0,0 +1,30 @@
+(ns palette.core
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(defn setup []
+  ; Set frame rate to 1 frames per second.
+  (q/frame-rate 1)
+  ; Set color mode to HSB (HSV) instead of default RGB.
+  (q/color-mode :hsb))
+
+(defn draw []
+  (q/no-loop)
+  (q/background 10)
+  (q/with-translation [110 110]
+    (q/fill (rand-int 256) 200 255)
+    (q/ellipse 20 20 210 210)
+    (q/fill (rand-int 256) 200 255)
+    (q/ellipse 260 20 210 210)
+    (q/fill (rand-int 256) 200 255)
+    (q/ellipse 20 260 210 210)
+    (q/fill (rand-int 256) 200 255)
+    (q/ellipse 260 260 210 210)))
+
+
+(defn -main [& args]
+  (q/defsketch palette
+    :title "The Sleepycat Collection"
+    :size [500 500]
+    :setup setup
+    :draw draw))
